2345b5393a68b6412f84c3a994af8284386b4640
Obito <2207625810@qq.com>
2018-08-26 17:37:52 +0800

    Changed thranslation button positon.

--- app/src/main/java/com/obito/recitewords/activitys/MainActivity.java
+++ app/src/main/java/com/obito/recitewords/activitys/MainActivity.java
@@ -1,11 +1,17 @@
 package com.obito.recitewords.activitys;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -47,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
         }
     };
     //添加数据的按钮
-    public FloatingActionButton fab;
+    public FloatingActionButton addWordFab;
+    //翻译按钮
+    public FloatingActionButton translateFab;
     //显示数据的List
     public ListView listView;
     //无数据时显示的text
     public TextView textView;
-    //在线翻译的布局按钮
-    public LinearLayout online;
     //数据源
     public List<Words> dataList;
     //适配器
@@ -67,15 +73,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //判断API版本是否高于安卓6.0 高就申请权限 否则无视
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+            requestPower();
+
         Bmob.initialize(this, "412b50fcd0b9d5c6fb33a982ae4b6d93");
         initView();
         initEvent();
         initInstance();
         downloadData();
+
     }
 
     private void initEvent() {
-        fab.setOnClickListener(new View.OnClickListener() {
+        addWordFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(MainActivity.this, AddActivity.class);
@@ -115,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        online.setOnClickListener(new View.OnClickListener() {
+        translateFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this,TranslateActivity.class);
@@ -149,10 +160,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+        addWordFab = (FloatingActionButton) findViewById(R.id.fab_add);
         listView = (ListView) findViewById(R.id.listview);
         textView = (TextView) findViewById(R.id.is_show);
-        online = (LinearLayout) findViewById(R.id.lin_online);
+        translateFab = (FloatingActionButton) findViewById(R.id.fab_translate);
         pb = (ProgressBar) findViewById(R.id.pb);
         pb.setVisibility(View.VISIBLE);
     }
@@ -172,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.m2:
                 AlertDialog.Builder adb = new AlertDialog.Builder(this);
                 adb.setTitle("啦啦啦");
-                adb.setMessage("欢迎使用本软件，以下为正确的使用姿势：\n1.点击右下角的圆形按钮来添加一个单词。\n2.长按已经添加的单词可删除。\n3.点击已经添加的单词可重新编辑。\n4.软件需要联网，数据储存在服务器，即使卸载软件，数据也不会丢失。\n5.求给个好评");
+                adb.setMessage("欢迎使用本软件，以下为正确的使用姿势：\n1.点击右下角的铅笔按钮来添加一个单词。\n2.点击右下角的放大镜按钮来翻译。\n2.长按已经添加的单词可删除。\n3.点击已经添加的单词可重新编辑。\n4.软件需要联网，数据储存在服务器，即使卸载软件，数据也不会丢失。\n5.求给个好评");
                 adb.setNegativeButton("哦。。。", null);
                 adb.show();
                 break;
@@ -183,6 +194,31 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return true;
+    }
+    public void requestPower() {
+        //判断是否已经赋予权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {//这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+            } else {
+                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE,}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       if (requestCode==1)
+       {
+           Intent intent=new Intent(MainActivity.this,WelcomeActivity.class);
+           startActivity(intent);
+           this.finish();
+       }
     }
 }
 
--- app/src/main/java/com/obito/recitewords/activitys/WelcomeActivity.java
+++ app/src/main/java/com/obito/recitewords/activitys/WelcomeActivity.java
@@ -1,8 +1,13 @@
 package com.obito.recitewords.activitys;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,26 +23,6 @@ import java.util.TimerTask;
  */
 
 public class WelcomeActivity extends Activity {
-    public int[] textArr = new int[]{R.string.text1,
-            R.string.text2,
-            R.string.text3,
-            R.string.text4,
-            R.string.text5,
-            R.string.text6,
-            R.string.text7,
-            R.string.text8,
-            R.string.text9,
-            R.string.text10,
-            R.string.text11,
-            R.string.text12,
-    };
-    public int[] img = new int[]{
-            R.mipmap.wbg1,
-            R.mipmap.wbg2,
-            R.mipmap.wbg3,
-            R.mipmap.wbg4,
-            R.mipmap.wbg5
-    };
     public LinearLayout ll;
     public TextView tv;
 
@@ -49,8 +34,8 @@ public class WelcomeActivity extends Activity {
         ll = (LinearLayout) findViewById(R.id.wll1);
         tv = (TextView) findViewById(R.id.wtv1);
         Random ran = new Random();
-        ll.setBackgroundResource(img[ran.nextInt(5)]);
-        tv.setText(textArr[ran.nextInt(12)]);
+        ll.setBackgroundResource(R.mipmap.wbg5);
+        tv.setText("愿你被世界温柔以待。");
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -59,5 +44,7 @@ public class WelcomeActivity extends Activity {
                 WelcomeActivity.this.finish();
             }
         }, 2500);
+
     }
+
 }
